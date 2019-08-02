0ae36fb91627d99f76af7bb3615f832cdd7f5529
wahyu <hyuarzani@gmail.com>
2019-08-02 13:58:24 +0700

    initial project

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-graph-express-multi-database-schema
+# EXAMPLE OF GRAPHQL APP USING EXPRESS FOR MULTIPLE DATABASE SCHEMA & USING OAUTH2
+
+## How to run
+
+Just do like node js application 
+
+`npm i`
+
+you need nodemon to serve
+
+`npm i -g nodemon`
+
+create .env file with params:
+
+    PORT={your port}
+    URL_EVENT={some event url}
+    URL_LATEST={some latest url}
+
+then you can run
+
+`npm run serve`
+
+or you can directly run the server using
+
+`node server.js`
+
+that's all
+
+## Debug
+
+Go to http://localhost:4000/graph
+
+those are graphiql UI to test your app
---
+++ schema.js
@@ -0,0 +1,83 @@
+const fetch = require('node-fetch');
+const {
+  GraphQLList,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLSchema,
+} = require('graphql');
+
+const LatestEvent = new GraphQLObjectType({
+  name: 'schedule',
+  description: '...',
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+      resolve: root => root.eventId
+    },
+    name: {
+      type: GraphQLString,
+      resolve: root => root.eventName
+    },
+    date: {
+      type: GraphQLString,
+      resolve: root => root.eventDate
+    }
+  })
+})
+
+const EventType = new GraphQLObjectType({
+  name: 'event',
+  description: '...',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      resolve: root => root.eventId
+    },
+    name: { 
+      type: GraphQLString,
+      resolve: root => root.eventName
+    },
+    imageUrl: { 
+      type: GraphQLString,
+      resolve: root => root.eventImageUrl
+    },
+    description: { 
+      type: GraphQLString,
+      resolve: root => root.eventDescription
+    },
+    term: { 
+      type: GraphQLString,
+      resolve: root => root.eventTerm
+    },
+  })
+});
+
+module.exports = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: '...',
+    fields: () => ({
+      event: {
+        type: EventType,
+        args:{
+          id: { type: GraphQLInt }
+        },
+        resolve: (root, args) => fetch(
+          `${process.env.URL_EVENT}${args.id}`
+        )
+        .then(response => response.json())
+      },
+      latest: {
+        type: GraphQLList(LatestEvent),
+        args: {
+          limit: { type: GraphQLInt },
+        },
+        resolve: (root, args) => fetch(
+          `${process.env.URL_LATEST}${args.limit}`
+        )
+        .then(response => response.json())
+      }
+    })
+  })
+})
